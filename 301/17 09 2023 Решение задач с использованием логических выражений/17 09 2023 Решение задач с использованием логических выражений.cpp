68c33a25fa0ff4fd2e851ac9f7d8cc66b54cb970
lostsky25 <lostsky25@yahoo.com>
2023-09-17 19:49:00 +0300

    .

---
+++ 301/17 09 2023 Решение задач с использованием логических выражений/17 09 2023 Решение задач с использованием логических выражений.cpp
@@ -0,0 +1,100 @@
+﻿#include <iostream>
+
+// Глобальная область
+//namespace ABC {
+//	int a = 10;
+//}
+//
+//namespace ABC2 {
+//	int a = 20;
+//}
+//
+//using namespace ABC;
+
+int main() {
+
+	// [5; 7]
+	//int x;
+	//std::cin >> x;
+
+	//if (x >= 5 && x <= 7) {
+	//	std::cout << "YES\n";
+	//}
+	//else {
+	//	std::cout << "NO\n";
+	//}
+
+
+	//int i = 0;
+
+	//i, j, k, c, r, l, count
+
+	// while (true);
+
+	// ================WHILE===============
+	//while (i < 10)
+	//{
+	//	std::cout << i << std::endl;
+	//	// i = i + 1; // i -= 1; 
+	//	// // i *= 1; // i %= 1; 
+	//	// // i /= 1;
+	//	i += 1; 
+	//}
+
+	// ================DO ... WHILE===============
+
+	//do {
+	//	std::cout << i << std::endl;
+	//	i += 1;
+	//} while (i > 10);
+
+
+	// ================FOR===============
+
+	//for (int j = 0, k = -1000; k < 0; k++, j++)
+	//{
+	//	std::cout << j << ' ' << k << std::endl;
+	//}
+
+
+	// ================SWITCH===============
+
+	//std::cout << "1. Play\n";
+	//std::cout << "2. Settings\n";
+	//std::cout << "3. Exit\n";
+
+	//std::cout << "Select item: ";
+
+	//int index;
+	//std::cin >> index;
+
+	//switch (index)
+	//{
+	//case 1:
+	//	std::cout << "Loading..." << std::endl;
+	//	break;
+	//case 2:
+	//	std::cout << "1. Graphics" << std::endl;
+	//	std::cout << "2. RAM" << std::endl;
+	//	std::cout << "3. Back" << std::endl;
+	//	break;
+	//case 3:
+	//	std::cout << "Finish" << std::endl;
+	//	break;
+	//}
+
+	// ================INCREMENT | DECREMENT===============
+
+	//int a = 10;
+
+	// a++; // postfix
+	// ++a; // prefix
+
+	// ================ARRAYS===============
+	
+	int a = 10;
+
+	std::cout << sizeof(a);
+
+	return 0;
+}
