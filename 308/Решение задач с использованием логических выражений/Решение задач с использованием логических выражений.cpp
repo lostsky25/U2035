b33fbc5f9176d5bb813b65191dcd7b12a672243b
lostsky25 <lostsky25@yahoo.com>
2023-11-01 19:59:14 +0300

    .

---
+++ 308/Решение задач с использованием логических выражений/Решение задач с использованием логических выражений.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+#include <bitset>
+
+int main() {
+
+    //int i = 9;
+
+    //while (i >= 0) {
+    //    std::cout << i << " ";
+    //    i--;
+    //}
+
+    //int a = 10; // 1010
+    //int b = 5;  // 0101
+
+    //std::bitset<16> bin_a(a);
+    //std::bitset<16> bin_b(b);
+
+    //std::cout << bin_a << std::endl;
+    //std::cout << "after shr\n";
+
+    //a = a >> 3;
+    //std::bitset<16> bin_result(a);
+    //std::cout << bin_result << std::endl;
+
+    //a = a << 3;
+    //std::bitset<16> bin_result2(a);
+    //std::cout << bin_result2 << std::endl;
+
+    //int a = 4;
+
+    //if () {
+    //    std::cout << "yes\n";
+    //}
+    //else {
+    //    std::cout << "no\n";
+    //}
+
+
+    //if () {
+    //    a = a + 1; // a += 1;
+    //}
+    //else if (a == 10) {
+    //    a = a * 10; // a *= 10;
+    //}
+    //else {
+    //    a = a - 1; // a -= 1;
+    //}
+    //std::cout << a << std::endl;
+
+
+    //unsigned forks = 0;
+    //unsigned spoons = 0;
+    //unsigned plates = 0;
+
+    //std::cout << forks << std::endl;
+    //std::cout << spoons << std::endl;
+    //std::cout << plates << std::endl;
+    //std::cout << "=============" << std::endl;
+
+    //std::cout << ++forks << std::endl;
+    //std::cout << ++spoons << std::endl;
+    //std::cout << ++plates << std::endl;
+    //std::cout << "=============" << std::endl;
+
+    //int a = 10;
+    //int b = a + 3;
+    //int c = a + b;
+    //std::cout << c << std::endl;
+
+    //float chocolate = 20.f; // 20.0f = 20.f
+    //float coffee = 30.5;
+    //float milk = 60.1;
+    //float sum = chocolate + coffee + milk;
+
+    //std::cout << sum << std::endl;
+
+	return 0;
+}
