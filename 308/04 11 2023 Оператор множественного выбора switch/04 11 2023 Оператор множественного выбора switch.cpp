bcc74b56939e36eefee1f3c8d5233c862cbae534
lostsky25 <lostsky25@yahoo.com>
2023-11-04 19:47:46 +0300

    .

---
+++ 308/04 11 2023 Оператор множественного выбора switch/04 11 2023 Оператор множественного выбора switch.cpp
@@ -0,0 +1,131 @@
+﻿#include <iostream>
+
+int main() {
+	// 'j' - symbol
+	// "qwerty" - string
+	
+	std::string str;
+	//std::string str2 = " zzzzzzzzzzzzz";
+
+	std::cin >> str;
+
+	/*for (int i = 0; i < str.size(); i++) {
+		std::cout << str[i] << std::endl;
+	}*/
+
+	std::cout << str << std::endl;
+
+	// std::cout << str << std::endl;
+
+	//std::cout << std::max(2, 3) << std::endl;
+	//std::cout << std::min(2, 3) << std::endl;
+	//std::cout << isdigit('8') << std::endl;
+	//std::cout << isalpha('b') << std::endl;
+	//std::cout << isalnum('b') << std::endl;
+	
+	//char sym;
+	//std::cout << "Enter a symbol:";
+	//std::cin >> sym;
+
+	//switch (sym) {
+	//case '0': 
+	//case '1': 
+	//case '2':
+	//case '3': 
+	//case '4': 
+	//case '5':
+	//case '6': 
+	//case '7': 
+	//case '8':
+	//case '9':
+	//	std::cout << "digit\n";
+	//	break;
+	//default:
+	//	std::cout << "alpha\n";
+	//	break;
+	//}
+	
+	//int day;
+
+	//std::cout << "Enter a day: ";
+	//std::cin >> day;
+
+	//switch (day)
+	//{
+	//case 1:
+	//	std::cout << "Monday\n";
+	//	break;
+	//case 2:
+	//	std::cout << "Tuesday\n";
+	//	break;
+	//case 3:
+	//	std::cout << "Wednesday\n";
+	//	break;
+
+	//default:
+	//	std::cout << "Error!";
+	//	break;
+	//}
+
+	// Ctrl + D - dublicate
+	// Ctrl + Z - undo
+	// Ctrl + Y - redo
+
+	//float a, b, result;
+	//char op;
+
+	//std::cout << "Enter a: ";
+	//std::cin >> a;
+
+	//std::cout << "Enter b: ";
+	//std::cin >> b;
+
+	//std::cout << "Enter operation: ";
+	//std::cin >> op;
+
+	//switch (op)
+	//{
+	//case '+':
+	//	result = a + b;
+	//	break;
+	//case '-':
+	//	result = a - b;
+	//	break;
+	//case '/':
+	//	if (b != 0) {
+	//		result = a / b;
+	//	}
+	//	else {
+	//		std::cout << "Error: Division by zero!\n";
+	//		return 2;
+	//	}
+
+	//	break;
+	//case '*':
+	//	result = a * b;
+	//	break;
+	//default:
+	//	std::cout << "Error: Incorrect operation!\n";
+	//	return 1;
+	//}
+	//
+	//std::cout << "Answer: " << result;
+
+	//int value;
+	//std::cin >> value;
+
+	//switch (value) {
+	//case 1: case 2:
+	//	std::cout << value + 1 << std::endl;
+	//	break;
+	//case 3:
+	//	std::cout << value + 3 << std::endl;
+	//	break;
+
+	//default:
+	//	std::cout << value<< std::endl;
+	//	break;
+	//}
+
+	return 0;
+}
