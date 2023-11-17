ac389dda69cd232c2911a0cc1633c10003a7a104
lostsky25 <lostsky25@yahoo.com>
2023-11-17 19:48:04 +0300

    .

---
+++ 1547/17 11 2023 Решение задач с циклом while/17 11 2023 Решение задач с циклом while.cpp
@@ -0,0 +1,81 @@
+﻿#include <iostream>
+
+int sum(int, int);
+int sub(int, int);
+int mul(int, int);
+int _div(int, int);
+
+int countVowels(std::string);
+
+int main() {
+
+    std::string str = "qwerty";
+
+    std::reverse(str.begin(), str.end());
+
+    std::cout << str;
+
+    //std::string str;
+    //
+    //std::cin >> str;
+
+    //std::cout << "Vowels count: " << countVowels(str) << std::endl;
+
+    //for (int i = 0; i < str.size(); i++) {
+    //    // tolower()
+    //    // toupper()
+    //    std::cout << (char)tolower(str[i]) << std::endl;
+    //}
+
+    // 'f' - character
+    // "s" - string (not std::string)
+
+    //str.push_back('q');
+
+    //std::cout << "Data: " << str << std::endl;
+    //std::cout << "Size: " << str.size() << std::endl;
+    //std::cout << "Length: " << str.length() << std::endl;
+
+    /*int a = 10;
+    int b = 20;
+
+    std::cout << sum(a, b) << std::endl;
+    std::cout << _div(a, 0) << std::endl;*/
+
+
+    return 0;
+}
+
+int countVowels(std::string data) {
+    int count = 0;
+
+    for (int i = 0; i < data.size(); i++) {
+        // a e i o u
+        if (tolower(data[i]) == 'a' || tolower(data[i]) == 'e' || tolower(data[i]) == 'i' ||
+            tolower(data[i]) == 'o' || tolower(data[i]) == 'u') {
+            count++;
+        }
+    }
+    return count;
+}
+
+int sum(int a, int b) {
+    return a + b;
+}
+
+int sub(int a, int b) {
+    return a - b;
+}
+
+int mul(int a, int b) {
+    return a * b;
+}
+
+int _div(int a, int b) {
+    if (b != 0) {
+        return a / b;
+    }
+    else {
+        return INT_MIN;
+    }
+}
---
+++ 1547/Циклы в программировании. Цикл while/Циклы в программировании. Цикл while.cpp
@@ -0,0 +1,101 @@
+﻿#include <iostream>
+#include <vector>
+
+int main() {
+    std::vector<int> arr;
+
+    for (int i = 0; i < 100; i++) {
+        arr.push_back(i + 1);
+    }
+
+    int target = 77;
+
+    /*for (int i = 0; i < arr.size(); i++) {
+        if (arr[i] == target) {
+            std::cout << i << std::endl;
+            break;
+        }
+    }*/
+
+    int l = 0;
+    int r = arr.size() - 1;
+    int m;
+
+    while (r - l > 1) {
+        m = (r + l) / 2;
+
+        if (arr[m] <= target) {
+            l = m;
+        }
+        else {
+            r = m;
+        }
+    }
+
+    if (arr[l] == target) {
+        std::cout << "index: " << l << " value: " << arr[l] << std::endl;
+    }
+    else {
+        std::cout << "Not found\n";
+    }
+
+    //std::vector<int> arr;
+
+    //for (int i = 0; i < 100; i++) {
+    //    arr.push_back(i + 1);
+    //}
+
+    //for (int i = 0; i < arr.size(); i++) {
+    //    std::cout << arr[i] << std::endl;
+    //}
+    
+    //unsigned long long n, ans;
+    //std::cin >> n;
+    //
+    //ans = 1;
+
+    //for (unsigned long long i = 1; i <= n; i++) {
+    //    ans = ans * i;
+    //}
+
+    //std::cout << ans;
+
+    //int n;
+    //std::cin >> n;
+
+    //for (int i = 1; i <= 10; i++) {
+    //    std::cout << n << "*" << i << "=" << n * i << std::endl;
+    //}
+
+    //int n, result;
+    //result = 0;
+
+    //std::cin >> n;
+    //
+    //for (int i = 1; i <= n; i++)
+    //    result += i;
+    //
+    //std::cout << result;
+
+    //for (int i = 0; i < 100; i++) {
+    //    std::cout << i;
+    //}
+
+    //int i = 0;
+
+    //do {
+    //    std::cout << i;
+    //    ++i;
+    //} while (i < 100);
+
+    /*int i = 0;
+    
+    while (i < 100) {
+        std::cout << i;
+        ++i;
+    }
+
+    std::cout << std::endl << i << std::endl;*/
+    
+    return 0;
+}
