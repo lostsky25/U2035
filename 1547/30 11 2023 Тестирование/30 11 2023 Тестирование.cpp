55960906d870d0b99c668bf8e37c7eff37dd4c4b
lostsky25 <lostsky25@yahoo.com>
2023-11-30 19:53:00 +0300

    .

---
+++ 1547/30 11 2023 Тестирование/30 11 2023 Тестирование.cpp
@@ -0,0 +1,146 @@
+﻿#include <iostream>
+#include <vector>
+
+//namespace N {
+//    int cout;
+//    int cout2;
+//}
+//
+//namespace N {
+//    int cout3;
+//}
+
+//void foo() {
+//    using namespace N;
+//    cout = 10;
+//    std::cout << N::cout;
+//}
+
+//using N::cout2;
+//using std::vector;
+//
+//using vvi = std::vector<std::vector<int>>;
+//
+//template <class T>
+//using vv = std::vector<std::vector<T>>;
+
+#include <bitset>
+
+int main() {
+    //srand(time(0));
+
+    //std::cout << rand() % 100 << std::endl;
+    
+    //int a = 10;
+    //std::bitset<32> a_bin(a);
+    //std::cout << a_bin << std::endl;
+    //
+    //std::bitset<32> a_bin2(a >> 2);
+    //std::cout << a_bin2 << std::endl;
+
+    //int a = 10;
+    //int b = 12;
+    //std::bitset<32> a_bin(a); // a
+    //std::bitset<32> b_bin(b); // b
+
+    //std::bitset<32> result_bin1(a & b);
+
+    //std::cout << "& - and\n";
+    //std::cout << a_bin << std::endl;
+    //std::cout << b_bin << std::endl;
+    //std::cout << result_bin1 << std::endl;
+
+    //std::bitset<32> result_bin2(a | b);
+
+    //std::cout << "\n| - or\n";
+    //std::cout << a_bin << std::endl;
+    //std::cout << b_bin << std::endl;
+    //std::cout << result_bin2 << std::endl;
+
+    //std::bitset<32> result_bin3(a ^ b);
+
+    //std::cout << "\n^ - xor\n";
+    //std::cout << a_bin << std::endl;
+    //std::cout << b_bin << std::endl;
+    //std::cout << result_bin3 << std::endl;
+
+    //std::bitset<32> result_bin4(~a);
+
+    //std::cout << "~ - not (inversion)\n";
+    //std::cout << result_bin4 << std::endl;
+
+    //N::cout3 = 10;
+
+    //vv<double> data;
+    //cout2 = 10;
+
+    //int max = 0;
+    //std::string str;
+    //std::cin >> str;
+
+    //int now = 0;
+
+    //for (int i = 0; i < str.size(); i++)
+    //{
+    //    if (str[i] == '0') {
+    //        now++;
+    //    }
+    //    else
+    //    {
+    //        if (now > max) max = now;
+    //        now = 0;
+    //    }
+    //}
+
+    //if (now > max) max = now;
+
+    //std::cout << max;
+
+    // str[i]
+
+    /*int n;
+    std::cin >> n;
+
+    int answ = 0;
+
+
+    if (n > 0) {
+        for (int i = 1; i <= n; i++) {
+            answ += i;
+        }
+    }
+    else {
+        for (int i = 1; i >= n; i--) {
+            answ += i;
+        }
+    }*/
+
+    //std::cout << answ;
+
+    /*while (1);
+    while (true) {}
+
+    for (;;);*/
+    //for (; true; std::cout << "1");
+
+    //int a = 5, b = 2;
+
+    //std::cout << (a > b) ? 1 : 2;
+
+
+    /*int index = 0;
+    std::cin >> index;
+
+    switch (index) {
+        case 1:
+            std::cout << 1;
+            break;
+        case 2:
+            std::cout << 2;
+            break;
+    }*/
+
+
+
+    return 0;
+}
---
+++ 308/29 11 2023 Тестирование/29 11 2023 Тестирование.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <vector>
+
+//void inc(int& value) {
+//    ++value;
+//}
+//
+//void print(std::vector<int>& data) {
+//    for (int i = 0; i < data.size(); ++i) {
+//        std::cout << data[i] << ' ';
+//    }
+//}
+//
+//void inc(std::vector<int>& data) {
+//    for (int i = 0; i < data.size(); ++i) {
+//        ++data[i];
+//    }
+//}
+
+int main() {
+    //std::vector<int> data(1000, 1);
+
+    //inc(data);
+
+    //print(data);
+
+    //int a = 10;
+
+    // int 4 bytes
+    // 
+    // |00000000|00000000|00000000|00001010|
+
+    //int* ptr_a = &a;
+
+    //std::cout << *ptr_a << std::endl;
+    //std::cout << a << std::endl;
+
+    //std::string str;
+
+    //std::cin >> str;
+
+    //int max, count = 0;
+    //for (int i = 0; i < str.size(); i++) {
+    //    if (str[i] == '0') {
+    //        ++count;
+    //        max = std::max(max, count);
+    //    }
+    //    else {
+    //        count = 0;
+    //    }
+    //}
+
+    //std::cout << max;
+
+    /*do while (true);
+    while (true);
+    for (;;);*/
+
+    return 0;
+}
