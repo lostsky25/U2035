73f9883ee52973c41a4a9da73f71fb9f0575e380
lostsky25 <lostsky25@yahoo.com>
2023-12-07 19:46:40 +0300

    .

---
+++ 1547/07 12 2023 Решение задач, указатели, ссылки/07 12 2023 Решение задач, указатели, ссылки.cpp
@@ -0,0 +1,128 @@
+﻿#include <iostream>
+
+//void swap(int& x, int& y) {
+//    int tmp = x;
+//    x = y;
+//    y = tmp;
+//}
+//
+//void swap(int* x, int* y) {
+//    int tmp = *x;
+//    *x = *y;
+//    *y = tmp;
+//}
+
+//void printlnStr(const char* str) {
+//    const char* now = str;
+//
+//    while (*now != '\0') {
+//        std::cout << *now;
+//        ++now;
+//    }
+//    std::cout << std::endl;
+//}
+
+//int sum(int a, int b) {
+//    return a + b;
+//}
+//
+//int sub(int a, int b) {
+//    return a - b;
+//}
+//
+//int mul(int a, int b) {
+//    return a * b;
+//}
+//
+//int _div(int a, int b) {
+//    return a / b;
+//}
+//
+//void printResult(int a, int b, int(*func)(int, int)) {
+//    std::cout << func(a, b);
+//}
+
+//void inc_n(int& value, int n) {
+//    value += n;
+//}
+
+//struct Point {
+//    int x;
+//    int y;
+//};
+//
+//Point getRandPoint() {
+//    Point tmp = { rand() % 100, rand() % 100 };
+//    return tmp;
+//}
+
+int main() {
+    //Point p1 = getRandPoint();
+
+    //std::cout << p1.x << ' ' << p1.y << std::endl;
+
+
+    //int value = 10;
+
+    //inc_n(value, 20);
+
+    //std::cout << value << std::endl;
+
+    //printResult(10, 20, sum); 
+    //std::cout << std::endl;
+
+    //printResult(10, 20, sub); 
+    //std::cout << std::endl;
+
+    //printResult(10, 20, mul); 
+    //std::cout << std::endl;
+
+    //printResult(10, 20, _div); 
+    //std::cout << std::endl;
+
+    //const char* str = "Hello, world!";
+    //const char str[] = {'c', 'b', 'a', '\0'};
+    
+    //printlnStr(str);
+
+    //int* arr = new int[10];
+
+    //for (size_t i = 0; i < 10; i++) {
+    //    arr[i] = i + 1;
+    //}
+
+    //for (size_t i = 0; i < 10; i++) {
+    //    std::cout << *(arr + i) << std::endl;
+    //}
+
+    //delete[] arr;
+
+    //int x = 10, y = 20;
+
+    //swap(&x, &y);
+
+    //std::cout << x << ' ' << y << std::endl;
+
+    //int a = 187398274;
+    
+    //char* ptr = (char*)&a;
+
+    //for (int i = 0; i < 4; ++i) {
+    //    std::cout << "0x" << (int*)(ptr + i) << " => " << (int)*(ptr + i) << std::endl;
+    //}
+
+    //int arr[] = { 10, 20, 30, 40, 50 };
+
+    //std::cout << typeid(arr).name() << std::endl;
+
+    // int* ptr = arr;
+
+    // *(ptr + 0) -> 10
+    // *(ptr + 1) -> 20
+
+    /*for (size_t i = 0; i < sizeof(arr) / sizeof(int); i++) {
+        std::cout << *(ptr + i) << std::endl;
+    }*/
+
+    return 0;
+}
---
+++ 308/06 12 2023 Решение задач/06 12 2023 Решение задач.cpp
@@ -0,0 +1,100 @@
+﻿#include <iostream>
+
+//void swap(int& a, int& b) {
+//    int tmp = a;
+//    a = b;
+//    b = tmp;
+//}
+
+//С++ использует новые методы работы с динамической памятью при помощи операторов new и delete:
+
+//new — для выделения памяти;
+//delete — для освобождения памяти. delete[] - если массив.
+//Оператор new используется в следующих формах :
+
+//new тип; — для переменных
+//new тип[размер]; — для массивов
+
+//int sum(int a, int b) {
+//    return a + b;
+//}
+//
+//int sub(int a, int b) {
+//    return a - b;
+//}
+//
+//void printlnResult(int a, int b, int(*func)(int, int)) {
+//    std::cout << func(a, b) << std::endl;
+//}
+
+//void inc_n(int& value, int n) {
+//    value += n;
+//}
+
+int main() {
+
+    //int a = 10;
+    //inc_n(a, 20);
+
+    //std::cout << a << std::endl;
+
+    // ptr_func(10, 20);
+
+    //printlnResult(10, 20, sum);
+    //printlnResult(10, 20, sub);
+
+    //int a = 10;
+    //int b = 10;
+
+    //int* const ptr = &a;
+    //*ptr = 12312;
+    //ptr = &b;
+
+    //const char* s = "wqerty";
+    //const char* now = s;
+
+    //std::cout << "s: " << (int*)s << std::endl;
+    //std::cout << "now: " << (int*)now << std::endl;
+
+    //while (*now != '\0') {
+    //    std::cout << *now << std::endl;
+    //    ++now;
+    //}
+
+    //std::cout << "s: " << (int*)s << std::endl;
+    //std::cout << "now: " << (int*)now << std::endl;
+
+    //int* arr = new int[10];
+
+    //for (int i = 0; i < 10; ++i) {
+    //    arr[i] = i;
+    //}
+
+    //// std::cout << arr + 0 -> 0
+    //// std::cout << arr + 1 -> 1
+    //// ...
+
+    //for (int i = 0; i < 10; ++i) {
+    //    std::cout << *(arr + i) << std::endl;
+    //}
+
+    //delete[] arr;
+
+
+    /*int a = 10, b = 20;
+
+    swap(a, b);
+
+    std::cout << a << ' ' << b << std::endl;*/
+
+    // &, *
+
+    //int nums[] = { 1, 2, 3, 4, 5, 6 };
+    //int* ptr = &nums[0]; // = nums;
+
+    //for (int i = 0; i < 6; ++i) {
+    //    std::cout << *(ptr + i) << std::endl;
+    //}
+
+    return 0;
+}
