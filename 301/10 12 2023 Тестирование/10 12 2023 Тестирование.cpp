a8534402505cf7e42a7bc14fe007df832b27e1e0
lostsky25 <lostsky25@yahoo.com>
2023-12-10 19:51:49 +0300

    .

---
+++ 301/10 12 2023 Тестирование/10 12 2023 Тестирование.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+//class Car
+//{
+//public:
+//    int x;
+//
+//    void move() {
+//        // ...
+//    }
+//
+//    void setHP(int _hp) {
+//        if (...) {
+//
+//        }
+//    }
+//
+//private:
+//    int hp;
+//    int y;
+//
+//protected:
+//    int z;
+//};
+
+int main() {
+    //Car car;
+    ////Car* car = new Car;
+    //car.setHP(200);
+    //car.move();
+
+    return 0;
+}
+
