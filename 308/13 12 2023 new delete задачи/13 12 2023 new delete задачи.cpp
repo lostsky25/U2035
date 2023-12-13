c9a1b7a8baf3d4a229f72ae510d0f84babf3b62b
lostsky25 <lostsky25@yahoo.com>
2023-12-13 19:49:15 +0300

    .

---
+++ 308/13 12 2023 new delete задачи/13 12 2023 new delete задачи.cpp
@@ -0,0 +1,116 @@
+﻿#include <iostream>
+
+//__forceinline void rowReverse(int* row, const int sz) {
+//    for (int i = 0, j = sz - 1; i < j; ++i, --j)
+//        std::swap(row[i], row[j]);
+//}
+//
+//__forceinline void inv2d(int** arr, const int n, const int m) {
+//    for (int i = 0; i < n; ++i) {
+//        for (int j = 0; j < m; ++j) {
+//            arr[i][j] = arr[i][j] == 0 ? 1 : 0;
+//        }
+//    }
+//}
+
+int main() {
+    //srand(time(0));
+
+    //int n, m;
+    //std::cin >> n >> m;
+
+    //int** arr = new int*[n];
+
+    //for (int i = 0; i < n; ++i)
+    //    arr[i] = new int[m];
+
+    //for (int i = 0; i < n; ++i) {
+    //    for (int j = 0; j < m; ++j) {
+    //        arr[i][j] = rand() % 2;
+    //    }
+    //}
+
+    //for (int i = 0; i < n; i++) {
+    //    rowReverse(arr[i], m);
+    //}
+
+    //for (int i = 0; i < n; ++i) {
+    //    for (int j = 0; j < m; ++j) {
+    //        std::cout << arr[i][j] << ' ';
+    //    }
+    //    std::cout << std::endl;
+    //}
+    //std::cout << std::endl;
+
+    //inv2d(arr, n, m);
+
+    //for (int i = 0; i < n; ++i) {
+    //    for (int j = 0; j < m; ++j) {
+    //        std::cout << arr[i][j] << ' ';
+    //    }
+    //    std::cout << std::endl;
+    //}
+
+    //for (int i = 0; i < n; i++)
+    //    delete[] arr[i];
+    //
+    //delete[] arr;
+
+    //std::ios_base::sync_with_stdio(false);
+    //std::cin.tie(nullptr);
+    //std::cout.tie(nullptr);
+
+    //int n, m, k;
+    //std::cin >> n >> m >> k;
+
+    //int** arr = new int*[n + k];
+
+    //for (int i = 0; i < n + k; ++i)
+    //    arr[i] = new int[m + k];
+
+    //for (int i = 0; i < n + k; ++i)
+    //    for (int j = 0; j < m + k; ++j)
+    //        arr[i][j] = 0;
+
+    //for (int c = 0; c <= k; c++) {
+    //    for (int i = 0; i < n + c; ++i) {
+    //        for (int j = 0; j < m + c; ++j) {
+    //            std::cout << arr[i][j] << ' ';
+    //        }
+    //        std::cout << std::endl;
+    //    }
+
+    //    system("cls");
+    //}
+
+
+    //for (int i = 0; i < n + k; i++)
+    //    delete[] arr[i];
+    //
+    //delete[] arr;
+
+    //srand(time(0));
+
+    //int n;
+    //std::cin >> n;
+
+    //char* arr = new char[n];
+
+    //for (int i = 0; i < n; ++i) {
+    //    arr[i] = rand() % 127;
+    //}
+
+    //float answ = 0.0f;
+
+    //for (int i = 0; i < n; i++) {
+    //    answ += arr[i];
+    //}
+
+    //answ /= n;
+
+    //std::cout << "AVG: " << answ << std::endl;
+
+    //delete arr;
+
+    return 0;
+}
