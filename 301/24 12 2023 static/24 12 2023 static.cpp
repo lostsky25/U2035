0567b1dbf2db758df8287543cb5946a9f7699399
lostsky25 <lostsky25@yahoo.com>
2023-12-24 19:58:14 +0300

    .

---
+++ 301/24 12 2023 static/24 12 2023 static.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+
+//void counter() {
+//    static int count = 0;
+//    std::cout << ++count;
+//}
+
+//class A {
+//public:
+//    A() {
+//        std::cout << "A Constructor\n";
+//    }
+//
+//    ~A() {
+//        std::cout << "A Destructor\n";
+//    }
+//};
+//
+//class B {
+//public:
+//    B() {
+//        std::cout << "B Constructor\n";
+//    }
+//
+//    ~B() {
+//        std::cout << "B Destructor: " << ++count << std::endl;
+//    }
+//    
+//    static int count;
+//};
+//
+//int B::count = 0;
+
+//A B::a;
+//
+//void foo() {
+//    B b;
+//}
+
+//class A
+//{
+//public:
+//    A() {
+//        std::cout << "A Constructor\n";
+//    }
+//
+//    ~A() {
+//        std::cout << "A Destructor\n";
+//    }
+//
+//    static void foo() {
+//        std::cout << count << __FUNCTION__ << std::endl;
+//    }
+//
+//private:
+//    static int count;
+//};
+//
+//int A::count = 1;
+//
+//void foo() {
+//    A::foo();
+//}
+
+int main() {
+    //B b1, b2, b3;
+
+    //foo();
+
+    //std::cout << "--------------------------\n";
+    //for (int i = 0; i < 10; ++i)
+    //    counter();
+
+    //for (int i = 0; i < 10; i++) {
+    //    static int idx = 0;
+    //    while (idx < 5) {
+    //        std::cout << idx;
+    //        ++idx;
+    //    }
+    //}
+
+    return 0;
+}
